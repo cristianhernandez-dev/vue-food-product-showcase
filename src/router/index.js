536ad994b1e6fba862c7c0049b0a9b260e54892c
cristianhernandez-dev <cristianhernandez@live.cl>
2026-04-11 20:17:32 -0400

    Corrige router hash history

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import ProductDetailView from "../views/ProductDetailView.vue";
 import FavoritesView from "../views/FavoritesView.vue";
@@ -23,7 +23,7 @@ const routes = [
 ];
 
 const router = createRouter({
-history: createWebHashHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
